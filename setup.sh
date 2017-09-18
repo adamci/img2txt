e71339fdde20f778f27205ce7ce31baf1dd064cb
Adam Cimpeanu <adam.cimpeanu@aya.yale.edu>
2017-09-18 03:31:40 -0400

    setup notebook in svhn subfolder of docker image

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-docker create -it -p 8888:8888 -p 6006:6006 -v $(pwd):/notebooks --name auto-caption tensorflow/tensorflow
+docker create -it -p 8888:8888 -p 6006:6006 -v $(pwd):/notebooks/svhn --name auto-caption tensorflow/tensorflow
 echo "Created docker container 'auto-caption' from tensorflow/tensorflow image"
